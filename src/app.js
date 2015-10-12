4ee43ec46b2029d8bb9e7bfad3788b70b4a43900
Steve Hogg <steve@collecto.io>
2015-10-13 12:36:42 +1300

    Fix React.js warnings about deprecated code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Foo from './Foo';
 
-React.render(<Foo/>, document.body);
+const app = document.createElement('div');
+document.body.appendChild(app);
+
+ReactDOM.render(<Foo/>, app);
