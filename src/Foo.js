b202b9f0acb1611223be12d26cdb7dddd3f54a9f
Steve Hogg <steve@collecto.io>
2015-10-13 12:20:22 +1300

    Hot reload with counter

--- src/Foo.js
+++ src/Foo.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class Foo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count:0};
+    setInterval(this.tick.bind(this), 1000);
+  }
+
+  tick() {
+    this.setState({count: this.state.count + 1});
+  }
+
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <div>
+        <h1>Hello</h1>
+        Count is {this.state.count}
+      </div>
+    )
   }
 }
 
