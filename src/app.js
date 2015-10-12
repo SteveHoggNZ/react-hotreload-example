0b8f8de92bd742b6b40efd28704706d024f2eb6b
Steve Hogg <steve@collecto.io>
2015-10-13 12:52:01 +1300

    Added CSS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Foo from './Foo';
 
+require('./app.css');
+
 const app = document.createElement('div');
 document.body.appendChild(app);
 
